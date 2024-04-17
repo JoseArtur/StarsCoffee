016a768edcbf0cdac4eb62a989b0d1601a254bdf
bellacolombarolli00 <up201902617@up.pt>
2024-04-17 17:04:46 +0100

    login endpoint

--- app/src/main/java/com/example/starscoffee/SignupActivity.kt
+++ app/src/main/java/com/example/starscoffee/SignupActivity.kt
@@ -8,9 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.starscoffee.databinding.ActivitySignupBinding
 import okhttp3.Call
 import okhttp3.Callback
-import okhttp3.FormBody
+import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import okhttp3.Response
 import java.io.IOException
 
@@ -28,44 +29,42 @@ class SignupActivity : AppCompatActivity() {
             val password = binding.editTextPassword.text.toString()
 
             if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, " enter email and password", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             login(email, password)
         }
     }
     private fun login(email: String, password: String) {
-        val url = "http://10.227.158.196:8090/coffee-shop/login" // bella url
-        val requestBody = FormBody.Builder()
-            .add("email", email)
-            .add("password", password)
-            .build()
+        val url = "http://10.227.158.196:8090/coffee-shop/login" //bella url
+        //val url = "http://localhost:8090/coffee-shop/login"
+        val json = "{\"email\":\"$email\", \"password\":\"$password\"}"
+        val requestBody = RequestBody.create(MediaType.parse("application/json"), json)
 
         val request = Request.Builder()
             .url(url)
             .post(requestBody)
+            .addHeader("Content-Type", "application/json")
             .build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onResponse(call: Call, response: Response) {
+                println("test error login message: ")
+                println(response.code())
                 if (response.isSuccessful) {
-                    val responseBody = response.body()?.string()
-                    // Aqui você pode usar responseBody como desejar, por exemplo, para extrair informações de usuário
-                    // e iniciar a atividade principal
                     val intent = Intent(this@SignupActivity, MainActivity::class.java)
                     startActivity(intent)
                     finish()
                 } else {
                     runOnUiThread {
-                        Toast.makeText(this@SignupActivity, "Login falhou", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@SignupActivity, "Login failed", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
 
             override fun onFailure(call: Call, e: IOException) {
-
                 runOnUiThread {
-                    Toast.makeText(this@SignupActivity, "Erro de rede: ${e.message}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@SignupActivity, "Network error: ${e.message}", Toast.LENGTH_SHORT).show()
                 }
             }
         })
